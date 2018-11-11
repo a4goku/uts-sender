dbdd4b51b5a7b3ff92126b12a875bf138f97a3f9
a4goku <wangyi_062500@hotmail.com>
2018-11-11 12:10:57 +0800

    创建通信协议

---
+++ src/main/java/uts/sender/protocol/Req.java
@@ -0,0 +1,44 @@
+package uts.sender.protocol;
+
+import java.io.Serializable;
+
+public class Req implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String type;
+    private String tag;
+    private String requestMessage;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getRequestMessage() {
+        return requestMessage;
+    }
+
+    public void setRequestMessage(String requestMessage) {
+        this.requestMessage = requestMessage;
+    }
+}
---
+++ src/main/java/uts/sender/protocol/Resp.java
@@ -0,0 +1,53 @@
+package uts.sender.protocol;
+
+import java.io.Serializable;
+
+public class Resp implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String type;
+    private String tag;
+    private String responseCode;
+    private String responseMessage;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getResponseCode() {
+        return responseCode;
+    }
+
+    public void setResponseCode(String responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
